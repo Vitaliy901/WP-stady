ae213613fd27dbeba2de2e89c0cb6e9923a2603f
Vitaliy901 <allamamos@gmail.com>
2025-07-19 13:11:35 +0300

    feat(theme): Initialisation WP Theme with components;
    - Created variables.scss for global style variables.
    - Added article-list.scss for styling the article list page.
    - Introduced article-page.scss for the article page layout.
    - Developed form.scss for form styling.
    - Created home.scss for the home page layout.
    - Added news-all.scss for the news overview page.
    - Implemented header and footer styles in their respective sections.
    - Added slick slider styles in slick.scss and slick-theme.scss.
    - Organized imports in style.scss to include all new styles.
    - Included sun animation styles for the article list header.

---
+++ wp-content/themes/runo-study/footer.php
@@ -0,0 +1,53 @@
+	<footer class="home-footer">
+		<div class="home-footer__wrapper">
+			<div>
+				<h3>Contact the Publisher</h3>
+				<address>
+					<a href="#">mike@runo.com</a>
+					<p>+944 450 904 505</p>
+				</address>
+			</div>
+			<div>
+				<h3>Explorate</h3>
+				<ul>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Partners</a></li>
+					<li><a href="#">Job Opportunities</a></li>
+					<li><a href="#">Advertise</a></li>
+					<li><a href="#">Membership</a></li>
+				</ul>
+			</div>
+			<div class="headquarter">
+				<h3>Headquarter</h3>
+				<address>
+					<span> 
+						191 Middleville Road,
+						NY 1001, Sydney
+						Australia
+					</span>
+				</address>
+			</div>
+			<div>
+				<h3>Connections</h3>
+				<ul class="home-footer-social">
+					<li><a class="icon-facebook" href="https://www.facebook.com" title="facebook" target="blank"></a></li>
+					<li><a class="icon-twitter" href="https://twitter.com" title="twitter" target="blank"></a></li>
+					<li><a class="icon-youtube" href="https://www.youtube.com" title="youtube" target="blank"></a></li>
+					<li><a class="icon-pinterest" href="https://www.pinterest.ru" title="pinteres" target="blank"></a></li>
+					<li><a class="icon-behance" href="https://www.behance.net/" title="behance" target="blank"></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="home-footer__cc-wrapper">
+			<span>&copy; 2021 | RUNO Publisher Studio</span>
+		</div>
+	</footer>
+
+    <?php 
+        wp_footer();
+    ?>
+	<script src="js/jquery-3.6.0.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/main.js"></script>
+	</body>
+</html>
---
+++ wp-content/themes/runo-study/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
+		<meta name="description" content="Article list"> 
+		<meta name="keywords" content="Articles">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link rel="preload" href="https://fonts.googleapis.com/css2?family=League+Spartan&family=Lora&family=Roboto&display=swap" as="style">
+		<link rel="preload" as="style" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css2?family=League+Spartan&family=Lora&family=Roboto&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>Home</title>
+		<link rel="shortcut icon" href="img/favicon/favicon.ico">
+
+        <?php 
+            wp_head();
+        ?>
+
+	</head>
+	<body class="home">
+
+	<header class="home-header">
+		<div class="header-wrapper">
+			<div class="article-header__inner-wrapper inner-wrapper--padding">
+				<div class="logo-wrapper logo-wrapper--article">
+					<a class="home-logo" href="index.html">RUNO</a>
+				</div>
+				<nav class="header-links">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="article-list.html">Articles</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div class="content-wr">
+			<div class="content-wr__container">
+				<h1>Richird Norton photorealistic rendering as real photos</h1>
+			</div>
+		</div>
+		<div class="slider fade">
+			<div>
+				<div class="image">
+						<div class="img one"></div>
+				</div>
+			</div>
+			<div>
+				<div class="image">
+					<div class="img two"></div>
+				</div>
+			</div>
+			<div>
+				<div class="image">
+						<div class="img three"></div>
+				</div>
+			</div>
+		</div>
+	</header>
